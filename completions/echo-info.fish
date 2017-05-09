75fb7e56d4d9c932fdf9d6f5a140828ddd7d1da6
David Fernandez <i.am.david.fernandez@gmail.com>
2017-05-09 21:08:19 +1000

    <DF> Added completions.

--- completions/echo-info.fish
+++ completions/echo-info.fish
@@ -5,3 +5,5 @@
 #
 # If your package doesn't provide any command line utility,
 # feel free to remove completions directory from the project.
+
+complete -c echo-info -l major -l minor -l warning -l error
